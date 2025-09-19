3f14d6ffe3e93f02143fc5e994a5ff27441e599e
Mircea-Pavel Anton <contact@mirceanton.com>
2025-09-20 00:25:58 +0300

    Remove emoji from app bar title for a cleaner appearance

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-toolbar-title> 📱 Remote Teleprompter </v-toolbar-title>
+      <v-toolbar-title>Remote Teleprompter </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
